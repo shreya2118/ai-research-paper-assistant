654c45c1c29ce41bdf38b69deeea56d2e9c31b81
shreya2118 <shreyareja18@gmail.com>
2026-05-29 21:49:52 +0530

    updated requirements

--- vector_store.py
+++ vector_store.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
